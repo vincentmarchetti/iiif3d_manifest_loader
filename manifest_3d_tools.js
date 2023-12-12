ae49aaf7d2a19748a655980eb38afae71f0d3df2
Vincent Marchetti <vmarchetti@aluminum.local>
2023-12-12 11:17:55 -0500

    Added some documentation

--- manifest_3d_tools.js
+++ manifest_3d_tools.js
@@ -1,5 +1,14 @@
 
+/*
+Initial function to determine if a Javascript object looks like
+an IIIF Annotation instance with criteria:
+-- the type attribute is string value "Annotation"
+-- the object has an attribute "body" which itself has type "Model"
 
+@obj -- a Javascript object, but generally this will be dictionary retrieved
+        from a json stream
+@returns  -- boolean
+*/
 function IsModel3DAnnotation( obj )
 {
     if ( obj.type == "Annotation" &&
@@ -8,6 +17,25 @@ function IsModel3DAnnotation( obj )
     return false;
 }
 
+/*
+A recursive search for a Annotation object somewhere in the tree of a 
+general javascript object.
+In practice, in the initial call to SearchForModel from client, the argument obj
+will be the javascript dictionary value of an IIIF Presentation API Manifest.
+
+The heart of identifying an Annotation is implemented in the IsModel3DAnnotation function.
+This search is independent of, and ignores, additional structure of the Manifest
+such as AnnotationPages or attributes of Canvas instances.
+ 
+This function will return the first Annotation object which is found and identified
+in the as a "3D" Annotation. The recursive search is depth-first.
+
+A javascript 'undefined' type is returned if no 3D Annotation is identified.
+
+@param obj A javascript object, in most cases a call to SearchForModel from client code
+the obj will be a IIIF Manifest object retrieved from a json stream
+@return an object that satisfies the IsModel3DAnnotation function or "undefined" type
+*/
 function SearchForModel( obj )
 {
     if (IsModel3DAnnotation( obj )) return obj;
